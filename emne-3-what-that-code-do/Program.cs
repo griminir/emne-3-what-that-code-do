eee577c9af8893c2956c078fcf9d7f9da425756b
griminir <viktor.elliot.king@gmail.com>
2024-05-02 20:21:57 +0200

    Add project files.

---
+++ emne-3-what-that-code-do/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("hva gjør denne koden? oppgave");
+
+var range = 250;
+var counts = new int[range];
+int allChars = 0;
+string text = "something";
+while (!string.IsNullOrWhiteSpace(text))
+{
+    text = Console.ReadLine();
+    foreach (var character in text.ToUpper() ?? string.Empty)
+    {
+        counts[(int)character]++;
+        allChars++;
+    }
+    for (var i = 0; i < range; i++)
+    {
+        if (counts[i] > 0)
+        {
+            var character = (char)i;
+            Console.WriteLine(character + " - " + (counts[i]/ (double)allChars) * 100 + '%');
+        }
+    }
+}
